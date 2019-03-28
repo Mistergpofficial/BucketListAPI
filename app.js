20e37354c19b76911e59c39365b40f865ba42f67
chuky4real <ehuriahgodspower@gmail.com>
2019-03-28 14:19:48 +0100

    Latest commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
@@ -21,8 +20,18 @@ mongoose.Promise = global.Promise
 
 
 
+app.all("*", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,XMLHttpRequest");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS, PATCH");
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+});
+
 app.use(morgan('dev'))
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/api/v1', require('./api/routes/user'));
