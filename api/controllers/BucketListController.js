ce8f0523b1681c909d8098b6733317011bdea69f
chuky4real <ehuriahgodspower@gmail.com>
2019-03-28 14:37:56 +0100

    Latest commit

--- api/controllers/BucketListController.js
+++ api/controllers/BucketListController.js
@@ -65,7 +65,21 @@ exports.add = (req, res) => {
         saveBucketList.save()
         .then(result => {
             if(result){
-            
+                let key1 = "bucketlists"
+                client.hmset(key1, [
+               'id' , saveBucketList._id,
+               'bucket_list_name', saveBucketList.bucket_list_name,
+               'full_name' , saveBucketList.full_name
+            ], function(err, resu) {
+                if(err){
+                    res.status(500).json({
+                        error: err
+                    })
+                }
+                res.status(200).json({
+                    message: resu
+                })
+            });
           let key = "bucketlists/search" + " " + req.body.bucket_list_name;
             client.hmset(key, [
            'id' , saveBucketList._id,
@@ -80,24 +94,7 @@ exports.add = (req, res) => {
             res.status(200).json({
                 message: reply
             })
-        });
-            
-        let key1 = "bucketlists"
-            client.hmset(key1, [
-           'id' , saveBucketList._id,
-           'bucket_list_name', saveBucketList.bucket_list_name,
-           'full_name' , saveBucketList.full_name
-        ], function(err, resu) {
-            if(err){
-                res.status(500).json({
-                    error: err
-                })
-            }
-            res.status(200).json({
-                message: resu
-            })
-        });
-        
+        }); 
         }else{
                 res.status(500).json('Could Not Be Saved');
             }
